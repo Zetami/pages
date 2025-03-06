626f293e49f6861a2b939b2a6df0c5a8b0d2d064
Zetami <joshuacrain00@gmail.com>
2025-03-05 20:47:05 -0800

    WIP make pages

--- CS102 project/js/script.js
+++ CS102 project/js/script.js
@@ -1,5 +1,26 @@
-document.getElementById("mobilenav").classList.toggle("hideburger");
+var mtopicsflag = 0;
+document.getElementById("mobilenav").classList.add("hideburger");
+document.getElementById("mtopicsDrop").classList.add("hidetopics");
 
 function burger(){
+    mtopicsflag = 0;
+    document.getElementById("mtopicsDrop").classList.add("hidetopics");
     document.getElementById("mobilenav").classList.toggle("hideburger");
+
+    document.getElementById("mtopics").style.backgroundColor = "#ffffa0";
+    document.getElementById("mtopics").style.textShadow = "none";
+}
+
+function mtopicsDrop(){
+    mtopicsflag ^= 1;
+    document.getElementById("mtopicsDrop").classList.toggle("hidetopics");
+
+    if(mtopicsflag == 1){
+        document.getElementById("mtopics").style.backgroundColor = "#ffc53d";
+        document.getElementById("mtopics").style.textShadow = "2px 2px 2px #ffffa0";
+    }
+    else{
+        document.getElementById("mtopics").style.backgroundColor = "#ffffa0";
+        document.getElementById("mtopics").style.textShadow = "none";
+    }
 }
